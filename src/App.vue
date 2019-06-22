152a90f0775d6caa39a90fd016b6e1e3b900de0d
Nadeem Shaikh <nadeem.bs@gmail.com>
2019-06-22 12:10:55 -0700

    Completed the module

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello-world></hello-world>
+    <!-- <hello-world></hello-world> -->
+    <book-list></book-list>
   </div>
 </template>
 
 <script>
+import BookList from './components/BookList';
 import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
+    BookList
   },
 };
 </script>
